2778bea48fd317fc7781c9a59d8eda80db0bdf9d
kentonself-appdynamics <ubuntu@ip-10-97-3-172.us-west-2.compute.internal>
2021-03-08 15:23:47 +0000

    Debugging.

--- hello.py
+++ hello.py
@@ -1,12 +1,14 @@
 import rook
+import logging
 
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    rook.flush()
     return 'Hello, World!\n'
 
 if __name__ == "__main__":
-    rook.start()
-    app.run(host="0.0.0.0", port=5000, threaded=True)
+    rook.start(log_to_stderr=logging.DEBUG)
+    app.run(host='0.0.0.0', port=5000, threaded=True)
