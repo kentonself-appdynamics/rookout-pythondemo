490def8086871f32318f8e0820f1d67a87db4996
kentonself-appdynamics <ubuntu@ip-10-97-3-172.us-west-2.compute.internal>
2021-03-18 19:40:54 +0000

    made a little more fun

--- hello.py
+++ hello.py
@@ -1,13 +1,23 @@
 import rook
 import logging
+import os
 
 from flask import Flask
 app = Flask(__name__)
 
+x = 0
+
 @app.route('/')
 def hello_world():
+    global x
+    if (x == 3):
+        x = 0
+    else:
+        x = x + 1
     rook.flush()
-    return 'Hello, World!\n'
+    print ("BLAH")
+    print (os.environ.get("ROOKOUT_LABELS"))
+    return 'Hello, World! ' + str(x) + ' \n'
 
 if __name__ == "__main__":
     rook.start(log_to_stderr=logging.DEBUG)
