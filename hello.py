6dee0f0b37fdef82a8e5cff6f236f77b04d442e0
kentonself-appdynamics <ubuntu@ip-10-97-3-172.us-west-2.compute.internal>
2021-03-30 21:56:17 +0000

    Changes for lesson

--- hello.py
+++ hello.py
@@ -9,15 +9,24 @@ x = 0
 
 @app.route('/')
 def hello_world():
+    isUserNew("Bartholomew", "Villavicencio", "Otorhinolaryngologist")
     global x
     if (x == 3):
         x = 0
     else:
         x = x + 1
-    rook.flush()
-    print ("BLAH")
-    print (os.environ.get("ROOKOUT_LABELS"))
     return 'Hello, World! ' + str(x) + ' \n'
+
+def isUserNew(last, first, occ):
+    #      SELECT UserID FROM users WHERE LastName = lastName    
+    #                                 AND FirstName = firstName
+    #                                 AND Occupation = occupation;
+    if (False):
+        print("addNewUser(lastName, firstName, occupation)")
+        return True;
+    print("user exists")
+    return False;
+
 
 if __name__ == "__main__":
     rook.start(log_to_stderr=logging.DEBUG)
